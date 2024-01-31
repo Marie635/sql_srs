1dc3983334c513eaccb175b2a734811599ce70a6
bieng <bienguig@gmail.com>
2024-01-31 22:20:01 +0100

    ajout d'une database avec les exercices et un memory state

--- app.py
+++ app.py
@@ -1,56 +1,39 @@
 import streamlit as st
-import pandas as pd
 import duckdb
-import io
 
-csv = '''
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-'''
-beverages = pd.read_csv(io.StringIO(csv))
 
-csv2 = '''
-food_item, food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-'''
-food_items = pd.read_csv(io.StringIO(csv2))
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-answer = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
-
-solution = duckdb.sql(answer).df()
+#solution = duckdb.sql(answer).df()
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review?",
-        ("Joins","GroupBy", "windows Functions"),
+        ("cross_joins","GroupBy", "windows Functions"),
         index=None,
         placeholder="Select a theme...",
 
     )
-    st.write('You selected:', option)
+    st.write('You selected:', theme)
+
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    st.write(exercise)
 
 st.header("enter your code:")
 query = st.text_area(label="votre code sql ici", key="user_input")
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-
-with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution)
-
-with tab3:
-    st.write(answer)
+# if query:
+#     result = duckdb.sql(query).df()
+#     st.dataframe(result)
+#
+# tab2, tab3 = st.tabs(["Tables", "Solution"])
+#
+# with tab2:
+#     st.write("table: beverages")
+#     st.dataframe(beverages)
+#     st.write("table: food_items")
+#     st.dataframe(food_items)
+#     st.write("expected:")
+#     st.dataframe(solution)
+#
+# with tab3:
+#     st.write(answer)
