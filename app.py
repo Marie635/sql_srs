420781ad6cd9b61711fc2df3e47ab9cc8027bc3a
bieng <bienguig@gmail.com>
2024-02-01 22:28:36 +0100

    encore

--- app.py
+++ app.py
@@ -7,16 +7,14 @@ import duckdb
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review?",
         ("cross_joins", "GroupBy", "window_functions"),
         index=None,
         placeholder="Select a theme...",
-
     )
-    st.write('You selected:', theme)
+    st.write("You selected:", theme)
 
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
@@ -34,13 +32,13 @@ if query:
     st.dataframe(result)
 
     try:
-        result=result[solution_df.columns]
+        result = result[solution_df.columns]
         st.dataframe(result.compare(solution_df))
     except KeyError as e:
         st.write("Some columns are missing")
 
     n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference!=0:
+    if n_lines_difference != 0:
         st.write(
             f"result has a {n_lines_difference} lines difference with the solution_df"
         )
@@ -58,4 +56,3 @@ with tab2:
 
 with tab3:
     st.write(answer)
-
